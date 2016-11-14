bdb8b56fcd1bceec428dcf47a9e2aec1ec92d37f
Ernest Semerda <ernest@semerda.com>
2016-11-13 20:39:59 -0800

    Panning Faces.

--- codepath-canvas/ViewController.swift
+++ codepath-canvas/ViewController.swift
@@ -34,55 +34,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    // Clicking on Excited face creates another face
-    @IBAction func onFaceGesture(_ sender: UIPanGestureRecognizer) {
-        let location = sender.location(in: view)
-        print("onFaceGesture.location = \(location)")
-        
-        // Tell which way a user is panning by looking at the gesture property
-        // Like translation, velocity has a value for both x and y components
-        // If the y component of the velocity is a positive value, the user is panning down.
-        // If the y component is negative, the user is panning up.
-        let velocity = sender.velocity(in: view)
-        print("onFaceGesture.velocity = \(velocity)")
-        
-        // This will tell us how far our finger has moved from the original "touch-down" point as we drag.
-        let translation = sender.translation(in: view)
-        print("onFaceGesture.translation = \(translation)")
-        
-        if sender.state == .began {
-            print("onExcitedGesture.Gesture began")
-            
-            // Gesture recognizers know the view they are attached to
-            let imageView = sender.view as! UIImageView
-            
-            // Create a new image view that has the same image as the one currently panning
-            newlyCreatedFace = UIImageView(image: imageView.image)
-            
-            // Add the new face to the tray's parent view.
-            view.addSubview(newlyCreatedFace)
-            
-            // Initialize the position of the new face.
-            newlyCreatedFace.center = imageView.center
-            
-            // Since the original face is in the tray, but the new face is in the
-            // main view, you have to offset the coordinates
-            newlyCreatedFace.center.y += trayView.frame.origin.y
-            
-            originalFaceCenter = newlyCreatedFace.center
-            
-        } else if sender.state == .changed {
-            print("onExcitedGesture.Gesture is changing")
-            
-            // Moves the element by x & y (ref translation)
-            newlyCreatedFace.center = CGPoint(x: originalFaceCenter.x + translation.x, y: originalFaceCenter.y + translation.y)
-            
-        } else if sender.state == .ended {
-            print("onExcitedGesture.Gesture ended")
-            
-        }
-    }
-
     // Tray slide up and down with down bounce
     // Ref: https://guides.codepath.com/ios/Using-Gesture-Recognizers
     @IBAction func onTrayPanGesture(_ sender: UIPanGestureRecognizer) {
@@ -126,6 +77,79 @@ class ViewController: UIViewController {
                     self.trayView.center = self.trayUp
                 }
             }
+        }
+    }
+    
+    // Clicking on Excited face creates another face
+    @IBAction func onFaceGesture(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        print("onFaceGesture.location = \(location)")
+        
+        // Tell which way a user is panning by looking at the gesture property
+        // Like translation, velocity has a value for both x and y components
+        // If the y component of the velocity is a positive value, the user is panning down.
+        // If the y component is negative, the user is panning up.
+        let velocity = sender.velocity(in: view)
+        print("onFaceGesture.velocity = \(velocity)")
+        
+        // This will tell us how far our finger has moved from the original "touch-down" point as we drag.
+        let translation = sender.translation(in: view)
+        print("onFaceGesture.translation = \(translation)")
+        
+        if sender.state == .began {
+            print("onExcitedGesture.Gesture began")
+            
+            // Gesture recognizers know the view they are attached to
+            let imageView = sender.view as! UIImageView
+            
+            // Create a new image view that has the same image as the one currently panning
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            
+            // Enable user interaction on the programatically created imageView so it can react when we apply Pan to it further down
+            newlyCreatedFace.isUserInteractionEnabled = true
+            
+            // Newly created face can be panned now by scaling the face
+            let panGesture = UIPanGestureRecognizer(target: self, action: #selector(onFaceScalePanGesture))
+            newlyCreatedFace.addGestureRecognizer(panGesture)
+            
+            // Add the new face to the tray's parent view.
+            view.addSubview(newlyCreatedFace)
+            
+            // Initialize the position of the new face.
+            newlyCreatedFace.center = imageView.center
+            
+            // Since the original face is in the tray, but the new face is in the
+            // main view, you have to offset the coordinates
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            
+            originalFaceCenter = newlyCreatedFace.center
+            
+        } else if sender.state == .changed {
+            print("onExcitedGesture.Gesture is changing")
+            
+            // Moves the element by x & y (ref translation)
+            newlyCreatedFace.center = CGPoint(x: originalFaceCenter.x + translation.x, y: originalFaceCenter.y + translation.y)
+            
+        } else if sender.state == .ended {
+            print("onExcitedGesture.Gesture ended")
+            
+        }
+    }
+    
+    // Ref: http://guides.codepath.com/ios/Using-Gesture-Recognizers#programmatically-add-and-configure-a-gesture-recognizer
+    func onFaceScalePanGesture(sender: UIPanGestureRecognizer) {
+        if sender.state == UIGestureRecognizerState.began {
+            print("onFacePan.Gesture began")
+            
+            UIView.animate(withDuration: 0.2, animations: {
+                sender.view?.transform = CGAffineTransform(scaleX: 3, y: 3)
+            })
+        } else if sender.state == UIGestureRecognizerState.ended {
+            print("onFacePan.Gesture ended")
+            
+            UIView.animate(withDuration: 0.2, animations: {
+                sender.view?.transform = CGAffineTransform(scaleX: 1, y: 1)
+            })
         }
     }
 
